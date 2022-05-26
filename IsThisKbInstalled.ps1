a193dd53c60bf85e5b5209583d089c95704ddc32
Baptiste Porte <baptiste.porte@cheops.fr>
2022-05-26 15:19:18 +0200

    Add description

--- IsThisKbInstalled.ps1
+++ IsThisKbInstalled.ps1
@@ -11,28 +11,47 @@ This script MUST be executed with an account that has access to
 the server list that you have entered. 
 
 .INPUTS
-None ATM
+ListServer
+ListKbToCheck
+
+.PARAMETER ListServer
+String list of which server you want to check
+
+.PARAMETER ListKbToCheck
+String list of which KB you want to check
 
 .OUTPUTS
-[Collections.Generic.List[OverAllocationInfo]] Retourne un tableau de données sous le format :
+[Collections.Generic.List[KbInfo]] Retourne un tableau de données sous le format :
 
-PS > .\DiskOveAlloc.ps1
-
-DriveLetter DriveTotalSpace DriveUsedSpace IsOverAllocated
------------ --------------- -------------- ---------------
-C:              42303746048   164179705856            True
-
-A noter que DriveTotalSpace et DriveUsedSpace sont en Byte. 
-
-.EXAMPLE
-PS> IsThisKbInstalled.ps1
+PS > .\IsThisKbInstalled.ps1
 
 ServerName   KbName    KbInstalled PendingReboot InstalledDate
 ----------   ------    ----------- ------------- -------------
-srv-hyperv01 KB4512578        True          True 09/07/2019 00:00:00
-srv-hyperv01 KB5013941       False          True 09/07/2019 00:00:00
-srv-hyperv02 KB4512578       False          True 09/07/2019 00:00:00
-srv-hyperv02 KB5013941        True          True 05/26/2022 00:00:00
+srv-hyperv01 KB4512578        True         False 09/07/2019 00:00:00
+srv-hyperv01 KB5013941       False         False None
+
+
+.EXAMPLE
+PS > .\IsThisKbInstalled.ps1 -ListServer "srv-hyperv01","srv-hyperv02","foo"
+Error on foo : NOT ACCESSIBLE.
+
+ServerName   KbName    KbInstalled PendingReboot InstalledDate
+----------   ------    ----------- ------------- -------------
+srv-hyperv01 KB4512578        True         False 09/07/2019 00:00:00
+srv-hyperv01 KB4589208       False         False None
+srv-hyperv01 KB5014022       False         False None
+srv-hyperv02 KB4512578       False         True  None
+srv-hyperv02 KB4589208        True         True  05/26/2022 00:00:00
+srv-hyperv02 KB5014022        True         True  05/26/2022 00:00:00
+
+.EXAMPLE
+PS > .\IsThisKbInstalled.ps1 -ListServer "srv-hyperv02","foo" -ListKbToCheck "KB4512578","KB7"
+Error on foo : NOT ACCESSIBLE.
+
+ServerName   KbName    KbInstalled PendingReboot InstalledDate
+----------   ------    ----------- ------------- -------------
+srv-hyperv02 KB4512578       False         False None
+srv-hyperv02 KB7             False         False None
 
 .LINK
 Online version: http://gitlab.infocheops.local/microsoft
@@ -40,12 +59,40 @@ Online version: http://gitlab.infocheops.local/microsoft
 #>
 
 Param (
-
+    [parameter(Mandatory = $FALSE, ValuefromPipeline = $True)]
+    [System.String[]]$ListServer,
+    [parameter(Mandatory = $FALSE, ValuefromPipeline = $True)]
+    [string[]]$ListKbToCheck
 
 )
 
 
 Begin {
+
+    ################################################################
+    #          You can HARD CODE your values here                  #
+    ################################################################
+
+    # Default value if no $ListServer parameter is set
+    if (!$ListServer) {
+        [System.String[]]$ListServer = 
+        "server-1",
+        "server-2",
+        "test"
+
+    }
+
+    # Default value if no $ListKbToCheck parameter is set
+    if (!$ListKbToCheck) {
+        [System.String[]]$ListKbToCheck = 
+        "KB4512578",
+        "KB5013941"
+    }
+
+    ################################################################
+    ################################################################
+
+
     # Shamely stolen from StackOverflow 
     # https://stackoverflow.com/questions/47867949/how-can-i-check-for-a-pending-reboot
     function Test-PendingReboot($Server) {
@@ -71,6 +118,7 @@ Begin {
             return $FALSE
         }
     }
+    # Test on local machine
     function Test-PendingRebootLocal($Server) {
         if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $TRUE }
         if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $TRUE }
@@ -97,17 +145,6 @@ Begin {
 
     $ListKbInfo = New-Object System.Collections.ArrayList
 
-    [System.String[]]$ListServer = 
-    "srv-hyperv01",
-    "srv-hyperv02",
-    "test"
-
-    [System.String[]]$ListKbToCheck = 
-    "KB4512578",
-    "KB5013941",
-    "KB4589208",
-    "KB5014022"
-
     [Bool]$PendingReboot = $FALSE
 
 }
@@ -115,9 +152,10 @@ Begin {
 Process {
     # Loop on the server list
     foreach ($Server in $ListServer) {
-        if ($env:COMPUTERNAME -match $Server){
+        if ($env:COMPUTERNAME -match $Server) {
             $PendingReboot = Test-PendingRebootLocal($Server)
-        }else{
+        }
+        else {
             $PendingReboot = Test-PendingReboot($Server)
         }
 
@@ -131,6 +169,7 @@ Process {
                     $KbInstalled = $TRUE
                     $InstalledDate = $QuickFixInfo.InstalledOn
                 }
+                # Then try with the class Win32_ReliabilityRecords
                 elseif ($QuickFixInfo = Get-WmiObject -Class Win32_ReliabilityRecords -Namespace root\cimv2 -ComputerName $Server -ErrorAction Stop |
                     Select-Object -Property @{LABEL = "InstallDate"; EXPRESSION = { $_.ConvertToDateTime($_.timegenerated) } } | 
                     Where-Object { $_.productname -match $ListKbToCheck[$i] } 
